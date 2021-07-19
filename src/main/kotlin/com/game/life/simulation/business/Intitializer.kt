6624e8d83519995eb93100eeb7c63851dadf1978
anteroserrano99 <serranoantero13@gmail.com>
2021-07-19 17:03:06 +0200

    fixed intialization

--- src/main/kotlin/com/game/life/simulation/business/Intitializer.kt
+++ src/main/kotlin/com/game/life/simulation/business/Intitializer.kt
@@ -2,18 +2,34 @@ package com.game.life.simulation.business
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 
 @Component
-class Intitializer {
+class Intitializer (
+        @Value("\${life.game.properties.length}") length: Int,
+        @Value("\${life.game.properties.height}") height: Int){
 
-    val logger: Logger = LoggerFactory.getLogger(Intitializer::class.java)
+    val length : Int = length
+    val height: Int = height
+
+
+
+
+
+val logger: Logger = LoggerFactory.getLogger(Intitializer::class.java)
 
 
     @PostConstruct
-    fun init() {
+    fun init(
+
+    ) {
         logger.info("I started")
+
+        for (i in 0..length) {
+            logger.info("I started $i")
+        }
     }
 
 
